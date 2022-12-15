dc4f1bc57faf1bada45f70f2025495fb1b079882
matuumdq <salinasmateo@outlook.com>
2022-12-15 15:38:43 -0300

    deployment

---
+++ src/pages/EditarCliente.jsx
@@ -0,0 +1,91 @@
+import { Form, useNavigate, useLoaderData, useActionData, redirect } from "react-router-dom"
+import Error from "../components/Error"
+import Formulario from "../components/Formulario"
+import { obtenerCliente, actualizarCliente } from "../data/clientes"
+
+
+export async function loader({params}){
+      
+      const cliente = await obtenerCliente(params.clienteId)
+      if(Object.values(cliente).length === 0){
+            throw new Response('', {
+                  status: 404,
+                  statusText: 'Cliente no Encontrado'
+            })
+      }
+     
+      return cliente
+
+}
+
+export async function action({request, params}){
+      const formData = await request.formData()
+      const datos = Object.fromEntries(formData)
+      
+      const email = formData.get('email')
+      
+      // Validacion
+      const errores = []
+      if (Object.values(datos).includes('')){
+          errores.push('Todos los campos son obligatorios')
+      }
+  
+      let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])")
+  
+      if (!regex.test(email)){
+          errores.push('El email no es valido')
+      }
+  
+      // retornar datos si hay errores
+      if(Object.keys(errores).length){
+          return errores
+      }
+  
+      // Actualizar el cliente
+      await actualizarCliente(params.clienteId, datos)
+  
+      return redirect('/')
+}
+
+function EditarCliente() {
+
+      const navigate = useNavigate()
+      const cliente = useLoaderData()
+      const errores = useActionData()
+
+  return (
+      <>
+      <h1 className='font-black text-xl text-blue-900'>Editar Cliente</h1>
+      <p className='mt-3'>A continuacion podras modificar los datos de un cliente</p>
+      <div className='flex justify-end'> 
+        <button 
+        className='bg-blue-800 text-white px-3 py-1 uppercase'
+        onClick={()=> navigate(-1)}>
+            Volver
+        </button>
+      </div>
+
+      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
+
+        {errores?.length && errores.map(( error, i) => <Error key={i}>{error}</Error>)}
+
+        <Form
+            method="POST"
+            noValidate
+        >
+            <Formulario
+                  cliente = {cliente}
+                  />
+
+            <input
+                type='submit'
+                className="mt-5 w-full bg-blue-800 uppercase p-3 text-white text-lg"
+                value='Guardar Cambios'
+            />
+        </Form>
+      </div>
+    </>
+  )
+}
+
+export default EditarCliente
